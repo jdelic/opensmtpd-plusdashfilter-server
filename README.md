6a583e0e483c928799f038965299347b9360fc0c
jdelic <jonas-github@maurus.net>
2016-03-08 01:45:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# OpenSMTPD-plusdashfilter
+
+So it turns out that OpenSMTPD, for boneheaded reasons, does not support
+`-` as a email *mailext* separator, but only supports `+`. For
+compatibility with *qmail*, for example, this project provides a
+SMTP filter daemon which can be used through OpenSMTPD's tagging
+mechanism to replace all dashes in an email address with pluses.
+
+This way "user-ext@example.com" becomes "user+ext@example.com" creating
+compatibility with other MTAs.
+
+## How do I use this?
+
+TODO: write this part :)
---
+++ plusdashfilter/server.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import argparse
+import daemonize
+import email.utils
+import gevent.monkey
+import smtplib
+from gsmtpd.server import SMTPServer
+
+
+_args = None
+
+
+class PlusDashFilterServer(SMTPServer):
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        rewritten_rcptros = []
+
+        # replace all "-" in email user parts with "+" for OpenSMTPD
+        for recipient in rcpttos:
+            name_address = email.utils.parseaddr(recipient)
+            if name_address[1].count('@') == 1 and name_address[1].split('@')[0].count('-') > 0:
+                new_address = '%s@%s' % (name_address[1].split('@')[0].replace('-', '+'),
+                                         name_address[1].split('@')[1])
+                name_address = (name_address[0], new_address)
+                rewritten_rcptros.append(email.utils.formataddr(name_address))
+            else:
+                rewritten_rcptros.append(recipient)
+
+        # now send the mail back to be processed
+        with smtplib.SMTP(_args.output_ip, _args.output_port) as smtp:
+            smtp.sendmail(mailfrom, rewritten_rcptros, data)
+
+
+def run():
+    server = PlusDashFilterServer(localaddr=(_args.input_ip, _args.output_ip))
+    server.serve_forever()
+
+
+def main():
+    gevent.monkey.patch_all()
+
+    _args = argparse.ArgumentParser(
+                description='This is a SMTP daemon that is used through OpenSMTPD configuration '
+                            'to filter email and replace "-" with "+" thereby making OpenSMTPD '
+                            'compatible with more widely deployed MTAs like qmail.'
+            )
+
+    grp_daemon = _args.add_argument_group('Daemon options')
+    grp_daemon.add_argument('-p', '--pidfile', dest='pidfile', default=None, help="Path to a pidfile")
+    grp_daemon.add_argument('-u', '--user', dest='user', default=None, help='Drop privileges and switch to this user')
+    grp_daemon.add_argument('-g', '--group', dest='group', default=None,
+                            help='Drop privileges and switch to this group')
+    grp_daemon.add_argument('-f', '--foreground', dest='foreground', default=False, action='store_true',
+                            help='Stay in foreground, do not fork')
+    grp_daemon.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
+                            help='Output extra logging (not implemented right now)')
+    grp_daemon.add_argument('-C', '--chdir', dest='chdir', default='/',
+                            help='Change working directory to the provided value')
+
+    grp_network = _args.add_argument_group('Network options')
+    grp_network.add_argument('--input-ip', dest='input_ip', default='127.0.0.1', help='The network address to bind to')
+    grp_network.add_argument('--input-port', dest='input_port', metavar='PORT', type=int, default=14001,
+                             help='The port to bind to')
+    grp_network.add_argument('--output-ip', dest='output_ip', default='127.0.0.1',
+                             help='The OpenSMTPD instance IP to return processed email to')
+    grp_network.add_argument('--output-port', dest='output_port', metavar='PORT', type=int, default=14000,
+                             help='THe port where OpenSMTPD listens for processed email')
+
+    _args.parse_args()
+
+    d = daemonize.Daemonize(
+            'plusdashfilterserver',
+            pid=_args.pidfile,
+            action=run,
+            user=_args.user,
+            group=_args.group,
+            verbose=_args.verbose,
+            foreground=_args.foreground,
+        )
+
+    d.start()
+
+
+if __name__ == '__main__':
+    main()
