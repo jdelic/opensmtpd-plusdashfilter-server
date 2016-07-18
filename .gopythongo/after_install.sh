e8c0c14fa586cae2a1352bac258d594560437716
Jonas Maurus <jonas-github@maurus.net>
2016-07-18 19:15:55 +0200

    add gopythongo configuration

---
+++ .gopythongo/after_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+test -e /etc/systemd/system/plusdashfilter.service || exit 1;
+
+if ! grep -q plusdashfilter /etc/group; then
+    groupadd plusdashfilter
+fi
+
+if ! grep -q plusdashfilter /etc/passwd; then
+    useradd -g plusdashfitler -s /bin/false -d /usr/local/plusdashfilter plusdashfilter
+fi
+
+systemctl --system daemon-relaod
+systemctl start plusdashfilter
+
