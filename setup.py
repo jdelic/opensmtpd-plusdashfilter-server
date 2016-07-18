bef74d4ffe4115b243cde467bacea43d3c7f0297
jdelic <jonas-github@maurus.net>
2016-07-18 18:30:32 +0200

    read version from plusdashfilter.__init__.version for convenience

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python -u
 import os
+import re
+
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 from pip.download import PipSession
 
-import time
-_version = "1.0.dev%s" % int(time.time())
-_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+with open("plusdashfilter/__init__.py", "rt", encoding="utf-8") as vf:
+    lines = vf.readlines()
 
+_version = "0.0.0+local"
+for l in lines:
+    m = re.match("version = \"(.*?)\"", l)
+    if m:
+        _version = m.group(1)
+
+_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 pipsession = PipSession()
 reqs_generator = parse_requirements(os.path.join(os.path.abspath(os.path.dirname(__file__)), "requirements.txt"),
                                     session=pipsession)  # prepend setup.py's path (make no assumptions about cwd)
