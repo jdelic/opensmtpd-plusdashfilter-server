7f493e932a3771ef45d89488200db0e6a8d588c3
jdelic <jonas-bitbucket@maurus.net>
2016-08-13 22:41:26 +0000

    let fpm/dpkg handle systemd stuff (using --deb-systemd)

--- .gopythongo/after_install.sh
+++ .gopythongo/after_install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-test -e /etc/systemd/system/plusdashfilter.service || exit 1;
-
 if ! grep -q plusdashfilter /etc/group; then
     groupadd plusdashfilter
 fi
@@ -9,7 +7,4 @@ fi
 if ! grep -q plusdashfilter /etc/passwd; then
     useradd -g plusdashfilter -s /bin/false -d /usr/local/plusdashfilter plusdashfilter
 fi
-
-#systemctl --system daemon-reload
-#systemctl start plusdashfilter
 
